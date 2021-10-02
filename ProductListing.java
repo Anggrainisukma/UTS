4c29a3e744594d2b268cbd83bd3ea0ee05694bd2
Anggraini Sukma Rahmawati <91783734+Anggrainisukma@users.noreply.github.com>
2021-10-02 18:44:05 +0700

    Delete ProductListing.java

--- ProductListing.java
+++
@@ -1,8 +0,0 @@
-package produk;
-
-public class productlisting {
-    public static void main(String[] args) {
-        product product_a = new product("product a");
-        product_a.getProductListing(5, 15, 10);
-    }
-}
